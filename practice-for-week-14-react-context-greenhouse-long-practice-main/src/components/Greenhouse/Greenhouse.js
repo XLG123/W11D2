b6c222707ac937cbeb9b9646fb965f00fb259884
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-07-11 13:59:23 -0400

    Phase I completed

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -1,16 +1,22 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
+  
+  const {themeName} = useTheme()
+
+  const img = themeName === "day" ? dayImage : nightImage 
+
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={img}
             alt='greenhouse' 
       />
       <LightSwitch />
--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/LightSwitch.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,25 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+
+  const {themeName, setThemeName} = useTheme();
+
+
+  const handleClick = e =>{
+    e.preventDefault();
+    // debugger
+    if(themeName === 'day'){
+      setThemeName('night')
+    } else {
+      setThemeName('day')
+    }
+  }
+  
   return (
     <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+      <div className="on" onClick={handleClick}>DAY</div>
+      <div className="off" onClick={handleClick}>NIGHT</div>
     </div>
   );
 }
